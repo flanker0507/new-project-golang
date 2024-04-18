bbbef9e03665c949d56ebd2325d0ea4214662cd3
flanker0507 <108620970+flanker0507@users.noreply.github.com>
2024-04-18 11:52:27 +0700

    simple program with go

--- yuda/main.go
+++ yuda/main.go
@@ -5,4 +5,24 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, Go")
 
+	fmt.Println("=================")
+
+	name := "Yuda Wira Pratama"
+	age := 23
+	isMentor := true
+	fmt.Println("Name: ", name)
+	fmt.Println("Umur: ", age)
+	fmt.Println("Is Mentor: ", isMentor)
+
+	fmt.Println("=================")
+
+	var radius float64 = 3.0
+	var area float64
+
+	const pi = 3.14
+
+	area = pi * (radius * radius)
+
+	fmt.Println(area)
+
 }
