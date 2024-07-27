b8364f4b8e4db526f0d2494b0a9527b719754728
flanker0507 <108620970+flanker0507@users.noreply.github.com>
2024-07-28 00:34:48 +0700

    for test

--- yuda/test.go
+++ yuda/test.go
@@ -1,5 +1,31 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	i := "Golang the best language"
+	//v:= []string{"a", "i", "u", "e", "o"}
+
+	for index, value := range i {
+		//fmt.Println("index: ", index, " value: ", string(value))
+
+		if index%2 == 0 {
+			fmt.Println("index: ", index)
+		}
+
+		if string(value) == "a" {
+			fmt.Println("value: ", string(value))
+		} else if string(value) == "i" {
+			fmt.Println("value: ", string(value))
+		} else if string(value) == "u" {
+			fmt.Println("value: ", string(value))
+		} else if string(value) == "e" {
+			fmt.Println("value: ", string(value))
+		} else if string(value) == "o" {
+			fmt.Println("value: ", string(value))
+		}
+
+	}
 
 }
